3870c1f00c252a26677d174b8ec57c7fa5dcb1eb
halenur,kar <358782@ogr.ktu.edu.tr>
2018-03-22 23:24:33 +0300

    class

---
+++ App_Code/Giris.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Giris
+/// </summary>
+public class Giris
+{
+   public class MyClass
+    {
+      static  public int Id { get; set; }
+      static  public string ad { get; set; }
+    }
+}
