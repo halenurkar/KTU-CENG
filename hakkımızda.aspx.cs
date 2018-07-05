c8cffa63354545a8b2be57735939345a6b2baa26
halenur,kar <358782@ogr.ktu.edu.tr>
2018-07-05 14:17:07 +0300

    hakkimizda sayfasi cs

---
+++ hakkımızda.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class hakkımızda : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void login_sayfasını_aç()
+    {
+        Response.Redirect("login.aspx");
+    }
+
+    protected void çıkış_butonu2(object sender, EventArgs e)
+    {
+        login_sayfasını_aç();
+    }
+}
