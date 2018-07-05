99f05c215b0a8f1e5bb53b76a56d677b1dbb8b2e
halenur,kar <358782@ogr.ktu.edu.tr>
2018-07-05 14:19:58 +0300

    iletisim sayfasi cs

---
+++ iletisim.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class iletisim : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    public void logine_gonder()
+    {
+         Response.Redirect("login.aspx");
+    }
+
+    protected void çıkış_yap(object sender, EventArgs e)
+    {
+        logine_gonder();
+    }
+}
