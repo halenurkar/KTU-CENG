fdc14ae067bd6119bff9c1d80f678439c5fdcd88
halenur,kar <358782@ogr.ktu.edu.tr>
2018-07-05 14:12:02 +0300

    dersler cs

---
+++ Dersler.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Dersler : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void sınıf1(object sender, EventArgs e)
+    {
+        Response.Redirect("sınıf1.aspx");
+    }
+
+    protected void sınıf2(object sender, EventArgs e)
+    {
+        Response.Redirect("sınıf2.aspx");
+    }
+
+    protected void sınıf3(object sender, EventArgs e)
+    {
+        Response.Redirect("sınıf3.aspx");
+    }
+
+    protected void sınıf4(object sender, EventArgs e)
+    {
+        Response.Redirect("sınıf4.aspx");
+    }
+}
