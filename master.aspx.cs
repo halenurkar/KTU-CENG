df73505107eae565452704914aaa5d5ea84610f1
halenur,kar <358782@ogr.ktu.edu.tr>
2018-07-05 14:44:39 +0300

    master page cs

---
+++ master.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class master : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    public void login_sayfasını_aç()
+    {
+        Response.Redirect("login.aspx");
+    }
+
+    protected void çıkış_butonu(object sender, EventArgs e)
+    {
+        login_sayfasını_aç();
+    }
+}
